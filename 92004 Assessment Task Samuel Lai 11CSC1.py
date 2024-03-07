71daed7f43c1eecee45bf4b04db5a0fd09819b9e
SAmuel <161664838+CocaCola203@users.noreply.github.com>
2024-03-07 14:50:55 +1300

    First day code
    Created greetings to the user and starting on dictionary1.

---
+++ 92004 Assessment Task Samuel Lai 11CSC1.py
@@ -0,0 +1,8 @@
+#create a score function
+score=0
+#dictionary function stores quesitons and answers
+dictionary={"Who is the protagonist of the movie ‘The Hobbit’?\n a) Gandalf\n b) Bilbo\n c) Thorin\n d) Smaug\n" : "b"}
+#Greet the user and ask their name
+print("Welcome to the Hobbit quiz!")
+name=input("What's your name?")
+print("Do you want to play this quiz" + name + "?")
